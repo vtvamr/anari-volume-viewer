f3a13493ba3160d2b1b25d0113a370279c4de3c9
Jefferson Amstutz <jamstutz@nvidia.com>
2023-11-03 13:20:01 -0500

    update default window layout

--- viewer.cpp
+++ viewer.cpp
@@ -38,26 +38,26 @@ static const char *g_defaultLayout =
     R"layout(
 [Window][MainDockSpace]
 Pos=0,25
-Size=1920,1174
+Size=1440,813
 Collapsed=0
 
 [Window][Viewport]
 Pos=551,25
-Size=1369,1174
+Size=889,813
 Collapsed=0
 DockId=0x00000003,0
 
 [Window][Lights Editor]
-Pos=0,614
-Size=549,585
+Pos=0,25
+Size=549,813
 Collapsed=0
-DockId=0x00000004,0
+DockId=0x00000002,1
 
 [Window][TF Editor]
 Pos=0,25
-Size=549,587
+Size=549,813
 Collapsed=0
-DockId=0x00000001,0
+DockId=0x00000002,0
 
 [Window][Debug##Default]
 Pos=60,60
@@ -65,11 +65,9 @@ Size=400,400
 Collapsed=0
 
 [Docking][Data]
-DockSpace     ID=0x782A6D6B Window=0xDEDC5B90 Pos=0,25 Size=1920,1174 Split=X
-  DockNode    ID=0x00000002 Parent=0x782A6D6B SizeRef=549,1174 Split=Y Selected=0xE3280322
-    DockNode  ID=0x00000001 Parent=0x00000002 SizeRef=549,587 Selected=0xE3280322
-    DockNode  ID=0x00000004 Parent=0x00000002 SizeRef=549,585 Selected=0x5098EBE6
-  DockNode    ID=0x00000003 Parent=0x782A6D6B SizeRef=1369,1174 CentralNode=1 Selected=0x13926F0B
+DockSpace   ID=0x782A6D6B Window=0xDEDC5B90 Pos=0,25 Size=1440,813 Split=X
+  DockNode  ID=0x00000002 Parent=0x782A6D6B SizeRef=549,1174 Selected=0xE3280322
+  DockNode  ID=0x00000003 Parent=0x782A6D6B SizeRef=1369,1174 CentralNode=1 Selected=0x13926F0B
 )layout";
 
 namespace viewer {
