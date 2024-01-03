7db928db6e742de255f39c36526475748d216803
Stefan Zellmann <info@szellmann.de>
2024-01-03 10:58:17 +0100

    Move structured field into FieldTypes header

--- FieldTypes.h
+++ FieldTypes.h
@@ -3,7 +3,36 @@
 
 #pragma once
 
+// std
 #include <array>
+#include <vector>
+
+// Structured field type //////////////////////////////////////////////////////
+struct StructuredField
+{
+  std::vector<uint8_t> dataUI8;
+  std::vector<uint16_t> dataUI16;
+  std::vector<float> dataF32;
+  int dimX{0};
+  int dimY{0};
+  int dimZ{0};
+  unsigned bytesPerCell{0};
+  struct
+  {
+    float x, y;
+  } dataRange;
+
+  bool empty() const
+  {
+    if (bytesPerCell == 1 && dataUI8.empty())
+      return true;
+    if (bytesPerCell == 2 && dataUI16.empty())
+      return true;
+    if (bytesPerCell == 4 && dataF32.empty())
+      return true;
+    return false;
+  }
+};
 
 // AMR field type /////////////////////////////////////////////////////////////
 typedef std::array<int, 6> BlockBounds;
--- readRAW.h
+++ readRAW.h
@@ -4,32 +4,8 @@
 #pragma once
 
 #include <stdio.h>
-
-struct StructuredField
-{
-  std::vector<uint8_t> dataUI8;
-  std::vector<uint16_t> dataUI16;
-  std::vector<float> dataF32;
-  int dimX{0};
-  int dimY{0};
-  int dimZ{0};
-  unsigned bytesPerCell{0};
-  struct
-  {
-    float x, y;
-  } dataRange;
-
-  bool empty() const
-  {
-    if (bytesPerCell == 1 && dataUI8.empty())
-      return true;
-    if (bytesPerCell == 2 && dataUI16.empty())
-      return true;
-    if (bytesPerCell == 4 && dataF32.empty())
-      return true;
-    return false;
-  }
-};
+// ours
+#include "FieldTypes.h"
 
 struct RAWReader
 {
